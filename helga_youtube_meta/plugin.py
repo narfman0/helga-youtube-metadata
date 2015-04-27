35192e1581429ad310c3a0d156e188ed31562c91
Jon Robison <narfman0@gmail.com>
2015-04-27 12:27:10 -0400

    -Update regex to fix videos in progress -Change to dummy likes vs dislikes

--- helga_youtube_meta/plugin.py
+++ helga_youtube_meta/plugin.py
@@ -3,18 +3,11 @@ import math
 from youtube_scraper.scraper import scrape_url
 from helga.plugins import match
 
-TEMPLATE = 'Title: {}, poster: {}, date: {}, views: {}, like ratio: {}'
+TEMPLATE = 'Title: {}, poster: {}, date: {}, views: {}, likes: {}, dislikes: {}'
 
-@match(r'(youtu\.be/\S+|youtube\.com/watch\?v=\S+)')
+@match(r'(youtu\.be/|youtube\.com/watch\?v=)\w+')
 def youtube_meta(client, channel, nick, message, match):
     """ Return meta information about a video """
     v = scrape_url('http://' + match[0])
-    if v.like and v.dislike:
-        ratio = "%.2f" % (v.like / float(v.dislike))
-    elif v.like:
-        ratio = 'infinite'
-    elif v.dislike:
-        ratio = '0'
-    else:
-        ratio = 'N/A'
-    return TEMPLATE.format(v.title, v.poster, v.published, v.views, ratio)
+    return TEMPLATE.format(v.title, v.poster, v.published, v.views, v.likes,
+                           v.dislikes)
