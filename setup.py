58a5a8e7dd896ed6bc88eff39d4fc73605810d2d
Jon Robison <narfman0@gmail.com>
2016-10-18 22:15:28 -0400

    -Capitulate to dumb stubborness

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from parse_requirements_not_suckily import parse_requirements
 from setuptools import setup, find_packages
 from helga_youtube_meta import __version__ as version
 
@@ -24,7 +23,10 @@ setup(
     include_package_data=True,
     py_modules=['helga_youtube_meta.plugin'],
     zip_safe=True,
-    install_requires=parse_requirements(),
+    install_requires=[
+        'python-dateutil',
+        'requests',
+    ],
     test_suite='',
     entry_points=dict(
         helga_plugins=[
